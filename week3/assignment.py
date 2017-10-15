238855047af95706e15b430e0696173487d5d47e
Max Aldunate <maxaldunate@gmail.com>
2017-10-15 12:36:46 +0200

    assignment week 3

--- week3/assignment.py
+++ week3/assignment.py
@@ -8,7 +8,7 @@ import socket
 
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mySocket.connect(('data.pr4e.org',80))
-cmd =   'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+cmd =   'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
 #cmd =   'GET http://google.com HTTP/1.0\r\n\r\n'.encode()
 #print(cmd)
 mySocket.send(cmd)
