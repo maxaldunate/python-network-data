24c49de522ca9795a6aa56f6d96d897e4c17866a
Max Aldunate <maxaldunate@gmail.com>
2017-10-15 10:54:02 +0200

    sockets.py fixed wirh \r\n

--- week3/sockets.py
+++ week3/sockets.py
@@ -8,9 +8,13 @@ import socket
 
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mySocket.connect(('data.pr4e.org',80))
-#cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.1\n'.encode()
+cmd =   'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+cmd =   'GET http://google.com HTTP/1.0\r\n\r\n'.encode()
+print(cmd)
 mySocket.send(cmd)
+
+print()
+print()
 
 while True:
     data = mySocket.recv(512)
