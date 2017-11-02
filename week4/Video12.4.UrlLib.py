31b9b904620328c11565483335bf627001cb69d0
Max Aldunate <maxaldunate@gmail.com>
2017-11-02 23:22:16 +0100

    read html with urllib

--- week4/Video12.4.UrlLib.py
+++ week4/Video12.4.UrlLib.py
@@ -1,9 +1,33 @@
 import urllib.request, urllib.parse, urllib.error
 
 def main():
+    ReadTxtFileFromWeb()
+    CountTxtFileFromWeb()
+    ReadHtmlFileFromWeb()
+    return
+
+def ReadHtmlFileFromWeb():
+    fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.html')
+    for line in fhand:
+        print(line.decode().strip())
+    print()
+
+def ReadTxtFileFromWeb():
     fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
     for line in fhand:
         print(line.decode().strip())
+    print()
+
+def CountTxtFileFromWeb():
+    fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+    counts = dict()
+
+    for line in fhand:
+        words = line.decode().split()
+        for word in words:
+            counts[word] = counts.get(word, 0) + 1
+        print(counts)
+    print()
 
 if __name__ == '__main__':
     main()
