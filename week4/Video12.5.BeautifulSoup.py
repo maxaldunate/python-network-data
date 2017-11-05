03f8f532b391efbe7c48a2ac80136eeabd0c0d55
Max Aldunate <maxaldunate@gmail.com>
2017-11-05 08:09:00 +0100

    beautiful soup

--- week4/Video12.5.BeautifulSoup.py
+++ week4/Video12.5.BeautifulSoup.py
@@ -1,33 +1,17 @@
 import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
 
 def main():
-    ReadTxtFileFromWeb()
-    CountTxtFileFromWeb()
-    ReadHtmlFileFromWeb()
+    url = input('Enter -')
+    html = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(html, 'html.parser')
+
+    tags = soup('a')
+    for tag in tags:
+        print(tag.get('href',None))
+
     return
 
-def ReadHtmlFileFromWeb():
-    fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.html')
-    for line in fhand:
-        print(line.decode().strip())
-    print()
-
-def ReadTxtFileFromWeb():
-    fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
-    for line in fhand:
-        print(line.decode().strip())
-    print()
-
-def CountTxtFileFromWeb():
-    fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
-    counts = dict()
-
-    for line in fhand:
-        words = line.decode().split()
-        for word in words:
-            counts[word] = counts.get(word, 0) + 1
-        print(counts)
-    print()
 
 if __name__ == '__main__':
     main()
